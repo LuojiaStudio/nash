8b0acb1b41a839875b075fa2ab3d7f26e235ea35
Jsceoz <jsceoz96@gmail.com>
2017-01-16 21:14:13 +0800

    edit db connection

--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+User.objects.all()
--- nash/settings.py
+++ nash/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'nash.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'whusu',
+        'USER': 'account_pg',
+        'PASSWORD': 'Wlhao123com',
+        'HOST': 'whusudatabase.pg.rds.aliyuncs.com',
+        'PORT': '3432',
     }
 }
 
